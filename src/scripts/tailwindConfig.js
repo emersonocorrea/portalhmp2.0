c93d5374419de053baf76ec7031539d8f76c9d66
emersonocorrea <110613926+emersonocorrea@users.noreply.github.com>
2024-07-25 15:08:56 -0300

    criado o menu

---
+++ src/scripts/tailwindConfig.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          secondary: '#159EEC',
+          accet: '#BFD2F8',
+          primary: '#1F2B6C',
+          black: '#212124'
+        },
+        spacing: {
+            '8px': '8px',
+            '16px': '16px',
+            '24px': '32px',
+            '48px': '48px',
+            '64px': '64px',
+            '92px': '92px',
+            '112px': '112px',
+            '124px': '124px',
+        },
+        fontFamily: {
+            'yeseva': ['Yeseva One', 'sans-serif'],
+            'work': ['Work Sans', 'sans-serif'],
+        }
+      }
+    }
+  }
